1741615fb0cef6a79e6e9b8abe60c775f90239b2
roushou <roushou9@gmail.com>
2024-06-01 15:45:43 +0700

    feat: add `cors`

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { databaseMiddleware } from "./middlewares/database";
 import { healthRouter } from "./routes/health";
 import { usersRouter } from "./routes/users";
 
 const app = new Hono();
+
+app.use("*", cors());
 
 app.route("/", healthRouter);
 
