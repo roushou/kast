e4796457cf2c115347f3826bff1bdcab966e7eba
roushou <roushou9@gmail.com>
2024-06-04 13:07:11 +0700

    chore: update seed script

--- apps/api/scripts/seed.ts
+++ apps/api/scripts/seed.ts
@@ -3,8 +3,8 @@ import type { CreateUser } from "../src/database/schemas";
 const users: CreateUser[] = [
   {
     address: "0x838aD0EAE54F99F1926dA7C3b6bFbF617389B4D9",
-    username: "John Doe",
-    description: "Hello World",
+    username: "Yukisaki",
+    description: `塊魂RTAします。耳も振ります。それ以外もします。Please Call me "Haru" I'm Katamari Speedrunnya! Tell your friends about me! ※Please don't eat my ears!! 連絡先 Contact➡ HarutomoRTA(at)gmail.com`,
   },
 ];
 
