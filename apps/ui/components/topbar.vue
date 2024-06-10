f8c25794ca51c43579c68cdc5e0b81c3dbc430b0
roushou <roushou9@gmail.com>
2024-06-10 14:15:41 +0700

    feat: add default layout

---
+++ apps/ui/components/topbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="h-12 px-12">
+    <ul class="h-full flex items-center space-x-4">
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">Market</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ apps/ui/layouts/default.vue
@@ -0,0 +1,4 @@
+<template>
+  <Topbar />
+  <slot />
+</template>
