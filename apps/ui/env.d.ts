61e2b935cc4cbdca37b79041239e661b284cfabb
roushou <roushou9@gmail.com>
2024-06-10 17:05:03 +0700

    feat(ui): add environment variables

---
+++ apps/ui/env.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NUXT_PUBLIC_API_BASE_URL: string;
+  }
+}
--- apps/ui/nuxt.config.ts
+++ apps/ui/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
+    },
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
 });
