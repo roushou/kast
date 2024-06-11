e6077aabc0b58878d33d2e119ba06db5d268c6d6
roushou <roushou9@gmail.com>
2024-06-11 14:12:12 +0700

    feat: add `Home` page

--- apps/ui/pages/index.vue
+++ apps/ui/pages/index.vue
@@ -1,5 +1,35 @@
+<script setup lang="ts">
+import type { Video } from "@kast/api";
+
+const config = useRuntimeConfig();
+
+const videosQuery = await useFetch<Video[]>(
+  `${config.public.apiBaseUrl}/videos`,
+);
+
+function getRelativeTime(date: string) {
+  const formatter = new Intl.RelativeTimeFormat("en", { style: "narrow" });
+  const since = new Date().getTime() - new Date(date).getTime();
+  const hours = new Date(since).getHours();
+  return formatter.format(-hours, "hour");
+}
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main class="mt-12 px-12">
+    <ul class="grid grid-cols-4 gap-6">
+      <li v-for="video in videosQuery.data.value">
+        <div class="w-full h-64 bg-gray-200"></div>
+        <div>
+          <p>{{ video.title }}</p>
+          <div class="flex justify-between">
+            <span class="text-sm">{{ video.views }} views</span>
+            <span class="text-sm">
+              {{ getRelativeTime(video.createdAt) }}
+            </span>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </main>
 </template>
