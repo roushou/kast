b9c66561f4743b56f13becf09f3a6183def4d53f
roushou <roushou9@gmail.com>
2024-06-17 22:09:27 +0700

    feat(ui): add `AccountDrawer` component

---
+++ apps/ui/components/account-drawer.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { useAccount, useBalance, useConnect } from "@wagmi/vue";
+import {
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+  VisuallyHidden,
+} from "radix-vue";
+import { shortenAddress } from "~/lib/utils";
+
+const accountQuery = useAccount();
+const balanceQuery = useBalance();
+const connectQuery = useConnect();
+console.log(balanceQuery.data.value);
+
+function connectWallet() {
+  const coinbaseWalletConnector = connectQuery.connectors.find(
+    (connector) => connector.id === "coinbaseWalletSDK",
+  );
+  connectQuery.connect({
+    /* biome-ignore lint/style/noNonNullAssertion: TODO */
+    connector: coinbaseWalletConnector!,
+  });
+}
+</script>
+
+<template>
+  <DialogRoot>
+    <DialogTrigger class="flex items-center space-x-2">
+      <img
+        :src="'https://avatar.vercel.sh/' + accountQuery.address.value"
+        alt="Account avatar"
+        class="w-6 h-6 rounded-full"
+      />
+      <span class="text-sm">{{ shortenAddress(accountQuery.address.value) }}</span>
+    </DialogTrigger>
+    <DialogPortal>
+      <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 z-40" />
+      <DialogContent class="fixed inset-y-0 overflow-hidden bg-white right-0 pt-4 pl-4 w-screen max-w-xs z-50">
+        <VisuallyHidden>
+          <DialogTitle>
+            Account drawer
+          </DialogTitle>
+        </VisuallyHidden>
+        <div class="flex flex-col space-y-4">
+          <div class="flex items-center space-x-2">
+            <img
+              :src="'https://avatar.vercel.sh/' + accountQuery.address.value"
+              alt="Account avatar"
+              class="w-6 h-6 rounded-full"
+            />
+            <span class="text-sm">
+              {{ shortenAddress(accountQuery.address.value) }}
+            </span>
+          </div>
+          <span class="text-lg">
+            {{ balanceQuery.data.value?.value ?? "0.00" }} ETH
+          </span>
+        </div>
+      </DialogContent>
+    </DialogPortal>
+  </DialogRoot>
+  <button
+    type="button"
+    v-if="!accountQuery.isConnected.value"
+    @click="connectWallet"
+  >
+    Connect
+  </button>
+</template>
--- apps/ui/components/topbar.vue
+++ apps/ui/components/topbar.vue
@@ -1,15 +1,22 @@
+<script setup lang="ts">
+import AccountDrawer from "./account-drawer.vue";
+</script>
+
 <template>
-  <nav class="h-12 px-12 flex items-center space-x-8 border-b border-b-gray-300">
-    <NuxtLink to="/">
-      <Logo />
-    </NuxtLink>
-    <ul class="h-full flex items-center space-x-8">
-      <li>
-        <NuxtLink to="/">Home</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/">Market</NuxtLink>
-      </li>
-    </ul>
-  </nav>
+  <div class="h-12 px-12 flex items-center justify-between space-x-8 border-b border-b-gray-300">
+    <nav class="h-full flex items-center space-x-8">
+      <NuxtLink to="/">
+        <Logo />
+      </NuxtLink>
+      <ul class="h-full flex items-center space-x-8">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/">Market</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <AccountDrawer />
+  </div>
 </template>
